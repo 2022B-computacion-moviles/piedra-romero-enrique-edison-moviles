c691e59da459ff562407acd1b630db98f5bddeac
Enrique Piedra <enrique.piedra@epn.edu.ec>
2023-02-19 11:29:54 -0500

    db02_ seccion nv5 menú correo

--- Deber2-Outlook/app/src/main/java/com/example/outlook/adapter/CorreoViewHolder.kt
+++ Deber2-Outlook/app/src/main/java/com/example/outlook/adapter/CorreoViewHolder.kt
@@ -1,7 +1,10 @@
 package com.example.outlook.adapter
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.example.outlook.Correo
 import com.example.outlook.R
@@ -15,6 +18,8 @@ class CorreoViewHolder (view: View) : RecyclerView.ViewHolder(view) {
     val receptor= view.findViewById<TextView>(R.id.correo_receptor)
     val mensaje= view.findViewById<TextView>(R.id.correo_mensaje)
 
+    val menuIcon= view.findViewById<ImageView>(R.id.menu_button)
+
 
     fun render(
         correoModel: Correo,
@@ -24,6 +29,31 @@ class CorreoViewHolder (view: View) : RecyclerView.ViewHolder(view) {
         receptor.text = correoModel.receptor
         mensaje.text = correoModel.mensaje
         itemView.setOnClickListener { onClickListener(correoModel)}
+
+
+
+        val popupMenu = PopupMenu(itemView.context, menuIcon)
+        popupMenu.menuInflater.inflate(R.menu.menu_correo, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_correo_mover -> {
+                    emisor.text = "movido"
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.menu_correo_eliminar -> {
+                    emisor.text = "eliminado"
+                    return@setOnMenuItemClickListener true
+                }
+                else -> return@setOnMenuItemClickListener false
+            }
+        }
+
+        menuIcon.setOnClickListener {
+            popupMenu.show()
+        }
+
+
 
     }
 }
