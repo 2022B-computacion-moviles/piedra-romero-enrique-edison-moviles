d0b19d3fbc15417e5d5bb017da43ab36e13366df
Enrique Piedra <enrique.piedra@epn.edu.ec>
2023-03-04 12:59:01 -0500

    Proyecto 2 - v5 rv

--- ProyectoFinal/app/src/main/java/com/example/proyecto/Home.kt
+++ ProyectoFinal/app/src/main/java/com/example/proyecto/Home.kt
@@ -1,17 +1,26 @@
 package com.example.proyecto
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyecto.adapter.RcVwAdapterInstruments
+import com.example.proyecto.data.entity.Instrument
 import com.example.proyecto.data.firebase.FirebaseGlobal
 import com.example.proyecto.data.firebase.FirebaseInstruments
 import com.example.proyecto.data.util.DataInstruments
 import com.google.firebase.auth.FirebaseAuth
 
 class Home : AppCompatActivity() {
+    private var selectedInstrumentCode: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -32,19 +41,56 @@ class Home : AppCompatActivity() {
             FirebaseGlobal.firebaseInstruments.create(instrument)
         }
 
-        val text_aux=findViewById<TextView>(R.id.view_instrument)
-        
-        FirebaseGlobal.firebaseInstruments.getAllInstruments { instruments ->
-            var aux=""
-            for (instrument in instruments) {
-                aux+=instrument.nombre +" ";
-            }
+        val rvInstruments=findViewById<RecyclerView>(R.id.rv_intruments)
 
-            text_aux.text=aux
+        FirebaseGlobal.firebaseInstruments.getAllInstruments { instruments ->
+            //var aux=""
+            //for (instrument in instruments) {
+            //    aux+=instrument.nombre +" ";
+            //}
+            initializeRecyclerView(instruments, rvInstruments)
+            registerForContextMenu(rvInstruments)
+
         }
 
 
 
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun initializeRecyclerView(
+        list: ArrayList<Instrument>,
+        recyclerView: RecyclerView
+    ) {
+        val adapter = RcVwAdapterInstruments(this, list)
+
+        recyclerView.adapter = adapter
+        recyclerView.itemAnimator = androidx.recyclerview.widget.DefaultItemAnimator()
+        recyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+        adapter.notifyDataSetChanged()
+    }
+
+
+
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_instrument_ver -> {
+                //val intent = Intent(this, ListCarro::class.java)
+                //intent.putExtra("selectedConcesionarioCode", selectedConcesionarioCode)
+                //startActivity(intent)
+                Toast.makeText(this, "Usuario seleccionado:", Toast.LENGTH_SHORT).show()
+
+                return true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+
+
+    fun setSelectedInstrumentCode(instrumentCode: Int) {
+        selectedInstrumentCode = instrumentCode
     }
 
 
